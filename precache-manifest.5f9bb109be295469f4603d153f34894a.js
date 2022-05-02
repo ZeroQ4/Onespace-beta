3a22602ea147f6739afd83bbe8ea9c46344100ab
1Spaceone <dotss87@gmail.com>
2022-05-02 15:08:10 +0300

    Updates

---
+++ precache-manifest.5f9bb109be295469f4603d153f34894a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0193edea447e7dc8a1abf7eca409e1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b7dfde0b62fd0037fca",
+    "url": "./static/css/main.ce990a87.chunk.css"
+  },
+  {
+    "revision": "83f96679c04bbbf9befc",
+    "url": "./static/js/2.2bf82390.chunk.js"
+  },
+  {
+    "revision": "aecfcf140955a7f4c0fa87a712b59f0f",
+    "url": "./static/js/2.2bf82390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7dfde0b62fd0037fca",
+    "url": "./static/js/main.910588b7.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  },
+  {
+    "revision": "8ad036823b56f99bdb9d21fb1293d573",
+    "url": "./static/media/discord.8ad03682.svg"
+  },
+  {
+    "revision": "ef2ea490f4a9f310f71419e8fd82f1b9",
+    "url": "./static/media/github.ef2ea490.svg"
+  },
+  {
+    "revision": "85be05786d14157e807e769b9eec9555",
+    "url": "./static/media/telegram.85be0578.svg"
+  },
+  {
+    "revision": "46c16738e16a4d0d5dccbb4f9bb3d11c",
+    "url": "./static/media/twitter.46c16738.svg"
+  }
+]);
